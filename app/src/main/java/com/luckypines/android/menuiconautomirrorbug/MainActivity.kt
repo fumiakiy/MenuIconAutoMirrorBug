650ccae8c1b6dca594046486afd928f73af3d6dc
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2021-02-19 11:57:40 -0500

    POC.

---
+++ app/src/main/java/com/luckypines/android/menuiconautomirrorbug/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.luckypines.android.menuiconautomirrorbug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu1, menu)
+        return true
+    }
+}
